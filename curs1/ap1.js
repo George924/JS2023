3f8514ca53eca7faac6ec57321ceee2fd1105432
Frontend JavaScript <frontendjavascript@C3-06.local>
2022-12-08 21:00:10 +0200

    curs

---
+++ curs1/ap1.js
@@ -0,0 +1,7 @@
+
+alert('Salut Jacascript')
+windows.alert('Salut')
+confirm('Vreau sa invat JavaScript')
+console.log('Mesaj in consola')
+console.info('Mesaj de informare')
+console.error('Mesaj de eroarex')

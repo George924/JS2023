3c4489d5df5e7301fc00bf2a0c1e4cb7f2c9f4d1
Frontend JavaScript <frontendjavascript@C3-06.local>
2023-01-19 20:53:08 +0200

    curs

---
+++ jasmine-standalone-4.5.0/spec/Math.js
@@ -0,0 +1,24 @@
+describe("helloWorld", () => {
+    it ("returns hello world", ()=> {
+        var actual = hello();
+        expect(actual).toBe("Hello world")
+    }
+})
+
+
+describe ("Suma", () => {
+    it("return sum 5 si 3 =8", () => {
+        expect(add(5,3)).toEqual(8);
+    });
+});
+describe("Suma reala",() => {
+  it("sum 1.4 si 0.7 = 2.1" , () => {
+    expect(add(1,4,0,7)).toEqual(2,1);
+  });
+});
+
+describe("Inmultire intregi" , ()=> {
+  it("return sum inm 5 si 3 = 15", () => {
+    expect((5,3)).toEqual(15);
+  });
+});
